d93a05ff00a1b69d345718833184ada558e50d88
Sarath M S <sarathmeledath2007@gmail.com>
2026-07-21 13:08:57 +0530

    18-07-2016 task complete

---
+++ 18-07-2026/array/test.js
@@ -0,0 +1,35 @@
+// Function to display favorite movies  
+
+let favoriteMovies = [
+    "Interstellar",
+    "Inception",
+    "The Dark Knight",
+    "Avengers: Endgame",
+    "3 Idiots"
+];
+
+console.log(favoriteMovies);
+
+// Create an array of your favorite foods
+
+let favoriteFoods = [
+    "Pizza",
+    "Sushi",
+    "Tacos",
+    "Burgers",
+    "Pasta"
+];
+
+console.log(favoriteFoods);
+
+// Create an array of your favorite colors
+
+let favoriteColors = [
+    "black",
+    "blue",
+    "green",
+    "red",
+    "purple"
+];
+
+console.log(favoriteColors);
---
+++ 18-07-2026/loops/test.js
@@ -0,0 +1,32 @@
+// Print five fruits using a loop
+
+let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// Print five countries using a loop
+
+let countries = ["India", "Japan", "Brazil", "Canada", "Germany"];
+
+for (let i = 0; i < countries.length; i++) {
+    console.log(countries[i]);
+}
+
+// Print five favorite games using a loop
+
+let games = ["Cricket", "Football", "Chess", "BGMI", "Minecraft"];
+
+for (let i = 0; i < games.length; i++) {
+    console.log(games[i]);
+}
+
+// Print only the first three items of one of your arrays.
+
+let fruit = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+for (let i = 0; i < 3; i++) {
+    console.log(fruit[i]);
+}
+
